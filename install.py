77d62c628da1db39d64bbbe2ba9242d8776461f3
JustANobody <42755661+justanobody2107@users.noreply.github.com>
2021-09-05 16:27:08 -0400

    Add files via upload

---
+++ install.py
@@ -0,0 +1,8 @@
+import os
+if os.path.exists("nekodl.py"):
+    os.remove("nekodl.py")
+os.system("curl -O https://raw.githubusercontent.com/justanobody2107/public-projects/main/nekodl.py")
+if os.path.exists("nekodl.py"):
+    os.remove("install.py")
+else:
+    print("\033[0;31m[ERROR]: Failed to install! Are you connected to the internet?\033[0m")
