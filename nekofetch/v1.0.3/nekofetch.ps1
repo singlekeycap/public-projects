02ab8d4d63d6216e41385c5eda16c8996652a8e3
JustANobody <42755661+justanobody2107@users.noreply.github.com>
2021-11-29 15:39:21 -0500

    Create nekofetch.ps1

---
+++ nekofetch/v1.0.3/nekofetch.ps1
@@ -0,0 +1,7 @@
+param (
+    [switch]$s = $True,
+    [switch]$n = $False,
+    [switch]$h = $False
+)
+If($h -eq $True){echo "Help menu"; echo "-n    | Uses NSFW images"; echo "-s    | Uses SFW images"; echo "-h    | Shows this menu"}
+Else{If($s -eq $True){$api = "https://nekos.life/api/v2/img/neko"; echo "Using SFW images"; Start-Sleep -Seconds 1; cls};If($n -eq $True){$api = "https://nekos.life/api/v2/img/lewd"; echo "Using NSFW images"; Start-Sleep -Seconds 1; cls};$url = ((iwr $api -UseBasicParsing).Content | ConvertFrom-Json).url;iwr $url -OutFile neko.jpg -UseBasicParsing;winfetch -image neko.jpg;Remove-Item neko.jpg}
