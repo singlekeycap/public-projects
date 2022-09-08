a62aa30de3165e8ec511f3503837e37cc4f59361
JustANobody <42755661+justanobody2107@users.noreply.github.com>
2022-09-08 17:31:15 -0400

    Delete test.ps1

--- test.ps1
+++
@@ -1,8 +0,0 @@
-param (
-    [switch]$s = $True,
-    [switch]$n = $False,
-    [switch]$h = $False
-)
-If($h -eq $True){echo "Help menu"; echo "-n    | Uses NSFW images"; echo "-s    | Uses SFW images"; echo "-h    | Shows this menu"}
-Else{If($s -eq $True){$api = "https://nekos.life/api/v2/img/neko"; echo "Using SFW images"; Start-Sleep -Seconds 1; cls};ElseIf($n -eq $True){$api = "https://nekos.life/api/v2/img/lewd"; echo "Using NSFW images"; Start-Sleep -Seconds 1; cls}}
-If($s -eq $True -or $n -eq $True){$url = ((iwr $api).Content | ConvertFrom-Json).url;iwr $url -OutFile neko.jpg;winfetch -image neko.jpg;Remove-Item neko.jpg}
